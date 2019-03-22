e154533faa5b900e118ae31bc533a8ec8abd0daa
DazhiZhong <zhongdz.dazhi2018@gdhfi.com>
2019-03-22 20:10:08 +0800

    Mar 22

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 # Create your views here.
 def homepage_view(request,*args,**kwargs):
-    print(request.user)
-    return render(request,'home1.html',{})
+    return redirect('Blog:users_login')
 
 def fuck_view(request,*args,**kwargs):
     return render(request,'fuck2.html',{})
